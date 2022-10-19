0c29829fa1e5995f845ffde8ad7fa809453d1513
jeeohly <giolee8@proton.me>
2022-10-19 14:03:59 -0400

    get access_token

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Strava Display
-in progress... 
+
+API link: https://developers.strava.com/docs/reference/<br>
+
+Functionalities so far: 
+- Dynamically retrieve new access token
--- strava.js
+++ strava.js
@@ -2,29 +2,33 @@ import * as dotenv from 'dotenv'
 import fetch from 'node-fetch'
 dotenv.config()
 
-const headers = {
+var headers = {
     'Accept': 'application/json, text/plain, */*',
     'Content-Type': 'application/json'
 }
 
-const body = JSON.stringify({
-    client_id: process.env.STRAVA_CLIENT_ID, 
-    client_secret: process.env.STRAVA_SECRET,
-    refresh_token: process.env.STRAVA_REFRESH_TOKEN,
-    grant_type: 'refresh_token'
-})
+async function getAccessToken(){
+    const body = JSON.stringify({
+        client_id: process.env.STRAVA_CLIENT_ID, 
+        client_secret: process.env.STRAVA_SECRET,
+        refresh_token: process.env.STRAVA_REFRESH_TOKEN,
+        grant_type: 'refresh_token'
+    })
 
-console.log(body)
+    const reauthrizeResponse = await fetch('https://www.strava.com/oauth/token', {
+        method: 'POST',
+        "headers": headers,
+        "body": body
+    })
+    const reauthJson = await reauthrizeResponse.json()
+    return reauthJson.access_token
+}
 
-const reauthrizeResponse = await fetch('https://www.strava.com/oauth/token', {
-    method: 'POST',
-    "headers": headers,
-    "body": body
-})
+console.log(await getAccessToken())
 
-const reauthJson = await reauthrizeResponse.json()
-const authToken = reauthJson.access_token
-console.log(authToken)
+
+
+
 
 
 
